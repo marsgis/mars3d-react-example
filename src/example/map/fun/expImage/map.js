159d6d16bd89f07819b7e1894cf2df47754e4d5a
wanghao <wh@marsgis.cn>
2022-10-09 13:12:17 +0800

    同步v3.4.9代码

--- src/example/map/fun/expImage/map.js
+++ src/example/map/fun/expImage/map.js
@@ -64,8 +64,8 @@ export function onUnmounted() {
 }
 
 // 查看场景出图
-export function showMapImg() {
-  return map.expImage({ download: false }).then((result) => {
+export function showMapImg(options = {}) {
+  return map.expImage({ download: false, ...options }).then((result) => {
     return result.image
   })
 }
@@ -79,6 +79,7 @@ export function downLoad() {
 export function downLoad2() {
   map.expImage({
     height: 300, // 指定 高度 或 宽度(指定1种就行，对应的自动缩放)
+    // width: 300, //同时指定后去裁剪中间部分
     download: true
   })
 }
