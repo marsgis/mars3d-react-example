5b96faf99a5840604a32361801bcb6377a016bf4
wanghao <wh@marsgis.cn>
2024-09-02 09:15:16 +0800

    同步代码

---
+++ src/example/thirdParty/quickearth/light/index.tsx
@@ -0,0 +1,10 @@
+import { MarsPannel, MarsButton, MarsGui } from "@mars/components/MarsUI"
+import { Space } from "antd"
+import { useState, useMemo } from "react"
+import * as mapWork from "./map.js"
+
+function UIComponent() {
+  return <MarsPannel visible={true} ></MarsPannel>
+}
+
+export default UIComponent
