ff528e5628236c70ab6a7c2fb87a3c5166ba880b
wanghao <wh@marsgis.cn>
2024-10-25 10:32:45 +0800

    同步代码

--- src/widgets/basic/GraphicEditor/map.ts
+++ src/widgets/basic/GraphicEditor/map.ts
@@ -1,16 +1,20 @@
 import * as mars3d from "mars3d"
 
 const Cesium = mars3d.Cesium
+
 let map: mars3d.Map // 地图对象
 
 // 初始化当前业务
 export function onMounted(mapInstance: mars3d.Map): void {
   map = mapInstance // 记录map
 }
-
 // 释放当前业务
 export function onUnmounted(): void {
   map = null
+}
+
+export function downloadFile(fileName: string, content: string) {
+  mars3d.Util.downloadFile(fileName, content)
 }
 
 export function julianToDate(julianToDate: any) {
@@ -23,3 +27,4 @@ export function getMapCurrentTime() {
 
   return { start: Cesium.JulianDate.toDate(start), stop: Cesium.JulianDate.toDate(stop) }
 }
+
