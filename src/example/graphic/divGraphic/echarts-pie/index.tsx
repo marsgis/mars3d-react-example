eec5551d878e6be0651b3ba588cd799d80a96fd7
wanghao <wh@marsgis.cn>
2024-10-29 22:29:42 +0800

    同步代码

---
+++ src/example/graphic/divGraphic/echarts-pie/index.tsx
@@ -0,0 +1,14 @@
+import { MarsPannel } from "@mars/components/MarsUI"
+import { GraphicLayerState } from "@mars/components/MarsSample/GraphicLayerState"
+
+function UIComponent() {
+  return (
+    <>
+      <MarsPannel visible={true} top={10} right={10}>
+        <GraphicLayerState />
+      </MarsPannel> 
+    </>
+  )
+}
+
+export default UIComponent
