307fb95d8c4dd07433033803604da88dbaeb945c
wanghao <wh@marsgis.cn>
2024-11-23 12:14:49 +0800

    同步代码

---
+++ src/example/graphic/entity/polygon-parallelogram/index.tsx
@@ -0,0 +1,16 @@
+import { MarsPannel } from "@mars/components/MarsUI"
+import { GraphicLayerState } from "@mars/components/MarsSample/GraphicLayerState"
+import { LocationTo } from "@mars/components/MarsSample/LocationTo"
+
+function UIComponent() {
+  return (
+    <>
+      <MarsPannel visible={true} top={10} right={10}>
+        <GraphicLayerState drawLabel1={"绘制面"} drawLabel2={"绘制立体面"} />
+      </MarsPannel>
+      <LocationTo />
+    </>
+  )
+}
+
+export default UIComponent
