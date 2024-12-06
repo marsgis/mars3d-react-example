d2b9e9da1677a6e80fcbd39b4f801cc1af6a72ec
wanghao <wh@marsgis.cn>
2024-12-06 11:18:18 +0800

    同步代码

--- src/widgets/basic/ManageBasemap/index.tsx
+++ src/widgets/basic/ManageBasemap/index.tsx
@@ -72,7 +72,7 @@ export default withLifeCyle(function (props) {
               mapWork.changeBaseMaps(item.id)
             }}
           >
-            <img className={styles.icon} src={item.options.icon || "//data.mars3d.cn/img/control/basemap/bingAerial.png"} alt="" />
+            <img className={styles.icon} src={item.options.icon || "//data.mars3d.cn/img/thumbnail/basemap/bingAerial.png"} alt="" />
             <span className="title">{item.name}</span>
           </li>
         ))}
